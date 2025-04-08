601f57383a8f3a6c468cb0987c8912765d7834fb
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-08 12:02:06 -0500

    new description

--- Add.java
+++ Add.java
@@ -4,8 +4,10 @@ public class Add {
     public static void main(String[] args) {
     int a=5,b=4;
     int sum=a+b;
+    int substract=a-b;
 
     System.out.println("\nAddition of two numbers" +sum);
+    System.out.println("Substraction of two numbers " +substract);
     }
 
     
