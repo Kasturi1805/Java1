c8d1cc1fcfbaaa304ea587458046436fc30e5775
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-08 11:55:26 -0500

    Description

---
+++ Add.java
@@ -0,0 +1,12 @@
+package Java1;
+
+public class Add {
+    public static void main(String[] args) {
+    int a=5,b=4;
+    int sum=a+b;
+
+    System.out.println("\nAddition of two numbers" +sum);
+    }
+
+    
+}
